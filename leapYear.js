677c5b8399ea3825676a3e1011ab0f6576a1b1df
cryster07 <93272418+cryster07@users.noreply.github.com>
2024-01-03 10:20:59 -0500

    Add files via upload

---
+++ leapYear.js
@@ -0,0 +1,17 @@
+var year = 2000;
+if(year%4===0){
+    if(year%100===0){
+        if(year%400===0){
+            console.log("Leap year.");
+        }
+        else{
+            console.log("Not leap year.")
+        }
+    }
+    else{
+            console.log("Leap year.")
+        }
+}
+   else{
+            console.log("Not leap year.")
+        }
