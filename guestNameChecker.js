d288b8de28a6c4ad80c917194798f4c897fd765e
cryster07 <93272418+cryster07@users.noreply.github.com>
2024-01-03 11:07:47 -0500

    Add files via upload

---
+++ guestNameChecker.js
@@ -0,0 +1,9 @@
+// Guess name Checker
+// .includes
+var guestList = ["safal","bibash","pk","sandesh"];
+if(guestList.includes(prompt("Enter the name to enter"))){
+    alert("lets go")
+}
+else{
+    alert("buy the ticket")
+}
